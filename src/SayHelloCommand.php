c35777a68e35f01fc2ecfa4b2600ed5d926cc552
Chris Nyaga <nyagachris75@gmail.com>
2017-11-07 18:36:24 +0300

    Updated the command line app with a simple hello world command.

---
+++ src/SayHelloCommand.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 11/7/17
+ * Time: 5:58 PM
+ */
+
+namespace Acme;
+
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class SayHelloCommand extends Command {
+    public function configure(){
+        $this->setName('sayHelloTo')
+             ->setDescription('Offer a greeting to the given person.')
+             ->addArgument('name', InputArgument::REQUIRED, 'Your Name.')
+             ->addOption('greeting',null, InputOption::VALUE_OPTIONAL,'Override the default greeting', 'Hello');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output){
+        $message = sprintf('%s, %s', $input->getOption('greeting'), $input->getArgument('name'));
+        $output->writeln("<info>{$message}</info>" );
+    }
+}
